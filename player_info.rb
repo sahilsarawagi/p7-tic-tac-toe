aca38b2f6590c0aa2981af738224cae6f88ab20d
sahil sarawagi <sahilsarawagi1@gmail.com>
2023-11-25 20:37:16 +0530

    - Added a PlayerInfo class to  manage player information.

---
+++ player_info.rb
@@ -0,0 +1,14 @@
+class PlayerInfo
+  def set_name=(name)
+    @name = name
+  end
+  def set_marker=(marker)
+    @marker=marker
+  end
+  def get_name
+    @name
+  end
+  def get_marker
+    @marker
+  end
+end
